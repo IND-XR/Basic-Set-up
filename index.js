cf613abb3f42232e767c82201d46dc560ae5b7e4
insta <anmolpreetsingh510@gmail.com>
2025-03-18 05:42:56 +0530

    Initialize Database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const path = require("path");  
-const chat = require("./models/chat.js")
+// const chat = require("./models/chat.js")
 
 main()
 .then((res)=>{
@@ -16,23 +16,6 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   }
-
-  let chat1 = new chat({
-    from : " neha",
-    to:"priyas",
-    msg :" send me your exam sheets",
-    created_at: new Date(),
-  })
-
-
-  chat1.save()
-  .then((res)=>{
-    console.log("run is running",res)
-  })
-  .catch((err)=>{
-    console.log("not working",err)
-  })
-
 
   const app = express();
   const PORT = 3000;
